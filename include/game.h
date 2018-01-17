4a87de2e19fcace767c7bb117515951cd6378440
Greg Donald <gdonald@gmail.com>
2018-01-17 06:56:24 -0600

    rework main

--- include/game.h
+++ include/game.h
@@ -14,16 +14,6 @@
 #include "dealer_hand.h"
 #include "player_hand.h"
 
-unsigned num_decks;
-unsigned money;
-unsigned current_player_hand;
-unsigned current_bet;
-
-Shoe shoe;
-DealerHand dealer_hand;
-PlayerHand player_hands[MAX_PLAYER_HANDS];
-
-int run(Shoe *shoe, DealerHand *dealer_hand, PlayerHand *player_hands);
-void deal_new_hand(Shoe *shoe, DealerHand *dealer_hand, PlayerHand *player_hands);
+void deal_new_hand();
 
 #endif
--- src/game.c
+++ src/game.c
@@ -4,29 +4,32 @@ unsigned num_decks = 1;
 unsigned money = 10000;
 unsigned current_bet = 500;
 
-void deal_new_hand(Shoe *shoe, DealerHand *dealer_hand, PlayerHand *player_hands)
+Shoe shoe = {};
+DealerHand dealer_hand;
+PlayerHand player_hands[MAX_PLAYER_HANDS];
+
+void deal_new_hand()
 {
-  if(need_to_shuffle(shoe))
+  if(need_to_shuffle(&shoe))
   {
-    shuffle(shoe);
+    shuffle(&shoe);
   }
 
   Card c1 = {};
-  dealer_hand->cards[0] = c1;
+  dealer_hand.cards[0] = c1;
   Card c2 = {};
-  dealer_hand->cards[1] = c2;
+  dealer_hand.cards[1] = c2;
 
   PlayerHand ph;
   player_hands[0] = ph;
-
 }
 
-int run(Shoe *shoe, DealerHand *dealer_hand, PlayerHand *player_hands)
+int main()
 {
-
-  deal_new_hand(shoe, dealer_hand, player_hands);
+  srand(time(NULL));
+  bt_off();
+  deal_new_hand(&shoe, &dealer_hand, player_hands);
+  bt_on();
 
   return 0;
 }
-
-
